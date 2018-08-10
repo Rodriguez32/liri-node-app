d9f3c7f04295130c8afb4d42577f99540a1669b0
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-08-09 21:20:26 -0500

    added functions

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,9 +1,39 @@
-console.log("This will work!!");
-
 require("dotenv").config();
 
-var keys = require("../javascript/keys");
+var keys = require("../javascript/keys.js");
+
+
+var client = new Twitter(keys.twitter);
+
+var myTweets = function() {
+    var Twitter = require("twitter");
+
+}
+
+var params = {
+    screen_name: "roman_azalea",
+    count: 20
+};
+
+client.get("https://twitter.com/roman_azalea",params,function(error,tweets,response){
+    if (error){
+        console.log("Error Ocurred");
+        } else {
+            console.log("My 20 Most Recent Tweets");
+            console.log("");
+        }
+});
+
+myTweets();
+
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
+// // The output does show what it says in random.txt
+// var fs = require("fs");
+
+// var data = fs.readFileSync("../random.txt");
+
+// console.log(data.toString());
+// console.log("Program Ended");
 
